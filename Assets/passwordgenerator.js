344c4d38fd44741119e70361cdf397228f569dc4
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2022-11-05 20:51:44 -0400

    recoded java
    Coded java to meet criteria

--- Assets/passwordgenerator.js
+++ Assets/passwordgenerator.js
@@ -1,10 +1,11 @@
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var CapLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var symbols = ['!', '@', "#", '%', '&', '*', '?', '^', '+', '='];
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const CapLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const symbols = ['!', '@', "#", '%', '&', '*', '?', '^', '+', '='];
 
+var selectedChar = ['']
 var Password = [''];
-
+console.log(selectedChar)
 
 function RandomIndex(a, b) {
     for (i = 0; i < a; i++) {
@@ -14,27 +15,29 @@ function RandomIndex(a, b) {
     }
 }
 
-function minCheck(a, b) {
-    var answer = window.prompt('How many ' + a + ' would you like in your password? (Min: ' + b + ') (Max:32)');
-    if (answer < b) {
-        window.alert('You must have a minimum of ' + b + ' ' + a + ' in your password!');
-        var result = minCheck(a, b);
+function checkChar() {
+    var numOfChar = window.prompt("How many character would you like in your password?")
+    if (numOfChar < 8) {
+        window.alert('You must have a minimum of 8 characters in your password!');
+        var result = checkChar();
         return result;
     }
-    else if (isNaN(answer)){
+    else if (isNaN(numOfChar)) {
         window.alert("You must select a number");
-        var result = minCheck(a, b);
+        var result = checkChar();
         return result;
     }
-    else if (answer >= 32){
-        window.alert("You can not have more than 32 " + a + " in your password.");
-        var result = minCheck(a, b);
+    else if (numOfChar > 128) {
+        window.alert("You cannot have more than 128 characters in your password");
+        var result = checkChar();
         return result;
     }
     else {
-        return answer;
+        return numOfChar;
     }
+
 }
+
 
 function shuffle(Password) {
     let currentIndex = Password.length, randomIndex;
@@ -46,26 +49,44 @@ function shuffle(Password) {
     return Password;
 }
 
-var generateBtn = document.querySelector('#generate');
+function charactertype(a, b) {
+    var answer = window.prompt('Would you like ' + a + ' in your password? Enter Yes or No)?')
+    var lanswer = answer.toLocaleLowerCase();
+    console.log(lanswer)
+    if (lanswer == 'yes') {
+        selectedChar = selectedChar.concat(b);
 
-function writePassword() {
-    Password = Password.splice()
-
-    var NumOfLetters = minCheck('lower case letters', 4);
-    RandomIndex(NumOfLetters, letters);
-    var NumofCapitaletters = minCheck('capital letters', 4);
-    RandomIndex(NumofCapitaletters, CapLetters);
-    var NumOfSymbols = minCheck('numbers', 1);
-    RandomIndex(NumOfSymbols, symbols);
-    var NumOfNumbers = minCheck('symbols', 1);
-    RandomIndex(NumOfNumbers, numbers);
-
-    var shuffledPassword = shuffle(Password);
-    var finalPassword = shuffledPassword.join('');
-  
-  var passwordText = document.querySelector('#password');
-  passwordText.value = finalPassword;
-
+    }
+    else if (lanswer == 'no') {
+       null;
+    }
+    else if (lanswer !=='yes'||'no'){
+        window.alert('Please enter Yes or No')
+        
+    }
 }
 
+
+function writePassword() {
+    Password = Password.splice();
+    var numOfChar = checkChar();
+
+    charactertype("lower case  letters", letters);
+    charactertype("upper case letters", CapLetters);
+    charactertype('numbers', numbers);
+    charactertype('symbols', symbols);
+
+    var shuffledChar = shuffle(selectedChar);
+    console.log(shuffledChar)
+    RandomIndex(numOfChar, shuffledChar);
+    console.log(numOfChar);
+    var shuffledPassword = shuffle(Password);
+    var finalPassword = shuffledPassword.join('');
+
+    var passwordText = document.querySelector('#password');
+    passwordText.value = finalPassword;
+}
+var generateBtn = document.querySelector('#generate');
 generateBtn.addEventListener("click", writePassword);
+
+
